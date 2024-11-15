265524a7a27483486a30afdc24f4c8f2e7c02ace
Sihan Fu <sf1524@ic.ac.uk>
2024-11-15 16:17:49 +0000

    extension contents done with added tests

--- src/main/kotlin/webserver/WebServer.kt
+++ src/main/kotlin/webserver/WebServer.kt
@@ -1,15 +1,82 @@
 package webserver
 
-// write your web framework code here:
+// write your web framework code her
+typealias HttpHandler = (Request)-> Response
+val routeconfig = listOf("/say-hello" to ::helloHandler,"/" to ::homePageHandler,"/computing" to ::homePageHandler,"/exam-marks" to requireToken("password1",::examMarkHandler))
 
-fun scheme(url: String): String = TODO()
+fun scheme(url: String): String = url.substringBefore("://")
 
-fun host(url: String): String = TODO()
+fun host(url: String): String = url.substringAfter("://").substringBefore("/")
 
-fun path(url: String): String = TODO()
+fun path(url: String): String = url.substringAfter(host(url)).substringBefore("?")
 
-fun queryParams(url: String): List<Pair<String, String>> = TODO()
+fun queryParams(url: String): List<Pair<String, String>> {
+  val querys = url.substringAfter("?", "")
+  if (querys.isEmpty())
+    return emptyList<Pair<String, String>>()
+  return querys.split("&").map { param ->
+    val (name, value) = param.split("=")
+    Pair(name, value)
+  }
+}
+
+fun route (request: Request): Response {
+  val pathstr = path(request.url)
+  var rtnval = Response(Status.NOT_FOUND, "Page not found!")
+  when (pathstr) {//why it's not called switch case :(
+    "/say-hello" -> rtnval = helloHandler(request)
+    "/" -> rtnval = homePageHandler(request)
+    "/computing" -> rtnval = homePageHandler(request)
+  }
+  return rtnval
+}
+
+fun configureRoutes (request: Request): HttpHandler {
+  val cfg = routeconfig.find{ config->
+    config.first == path(request.url)
+  }
+
+  if (cfg != null)
+    return cfg.second
+  return { request->
+    Response(Status.NOT_FOUND,"Page not found!")
+  }
+}
+
+fun requireToken(token: String, wrapped: HttpHandler): HttpHandler {
+  val func :HttpHandler = { request ->
+    if (request.authToken==token)
+      wrapped(request)
+    else
+      Response(Status.FORBIDDEN,"You don't have access to this page!")
+  }
+  return func
+}
 
 // http handlers for a particular website...
 
-fun homePageHandler(request: Request): Response = Response(Status.OK, "This is Imperial.")
+fun homePageHandler(request: Request): Response {
+  val pathstr = path(request.url)
+  var resp = "This is Imperial."
+  if (pathstr == "/computing")
+    resp = "This is DoC."
+  return Response(Status.OK, resp)
+}
+
+fun helloHandler(request: Request): Response {
+  var resp = "Hello, World!"
+  val params = queryParams(request.url)
+  val name = params.find { param ->
+    param.first == "name"
+  }
+  val style = params.find { param ->
+    param.first == "style"
+  }
+  if (name != null)
+    resp = "Hello, " + name.second + "!"
+  if (style != null && style.second == "shouting")
+    resp = resp.uppercase()
+  return Response(Status.OK, resp)
+}
+
+fun examMarkHandler(request: Request): Response = Response(Status.OK, "This is very secret.")
--- src/test/kotlin/webserver/WebServerTest.kt
+++ src/test/kotlin/webserver/WebServerTest.kt
@@ -1,14 +1,15 @@
 package webserver
 
+import com.sun.net.httpserver.HttpExchange
 import org.junit.Test
 import kotlin.test.assertEquals
 
 class WebServerTest {
-
   @Test
   fun `can extract scheme`() {
     assertEquals("http", scheme("http://www.imperial.ac.uk/"))
     assertEquals("https", scheme("https://www.imperial.ac.uk/"))
+    assertEquals("ftp", scheme("ftp://I.dont.actually.know.one/"))
   }
 
   @Test
@@ -16,6 +17,7 @@ class WebServerTest {
     assertEquals("www.imperial.ac.uk", host("http://www.imperial.ac.uk/"))
     assertEquals("www.imperial.ac.uk", host("https://www.imperial.ac.uk/"))
     assertEquals("www.imperial.ac.uk", host("https://www.imperial.ac.uk/computing"))
+    assertEquals("www.imperial.ac.uk", host("https://www.imperial.ac.uk/computing/programming"))
   }
 
   @Test
@@ -25,6 +27,7 @@ class WebServerTest {
     assertEquals("/computing", path("https://www.imperial.ac.uk/computing"))
     assertEquals("/computing/programming", path("https://www.imperial.ac.uk/computing/programming"))
     assertEquals("/computing", path("https://www.imperial.ac.uk/computing?q=abc"))
+    assertEquals("/computing/programming", path("https://www.imperial.ac.uk/computing/programming?q=abc"))
   }
 
   @Test
@@ -40,74 +43,83 @@ class WebServerTest {
 
 // ***** Tests for Handlers *****
 
-//  @Test
-//  fun `says hello world`() {
-//    val request = Request("http://www.imperial.ac.uk/say-hello")
-//    assertEquals("Hello, World!", helloHandler(request).body)
-//  }
+  @Test
+  fun `says hello world`() {
+    val request = Request("http://www.imperial.ac.uk/say-hello")
+    assertEquals("Hello, World!", helloHandler(request).body)
+  }
+
+  @Test
+  fun `can be customised with particular name`() {
+    val request = Request("http://www.imperial.ac.uk/say-hello?name=Fred")
+    assertEquals("Hello, Fred!", helloHandler(request).body)
+  }
 //
-//  @Test
-//  fun `can be customised with particular name`() {
-//    val request = Request("http://www.imperial.ac.uk/say-hello?name=Fred")
-//    assertEquals("Hello, Fred!", helloHandler(request).body)
-//  }
-//
-//  @Test
-//  fun `can process multiple params`() {
-//    val request = Request("http://www.imperial.ac.uk/say-hello?name=Fred&style=shouting")
-//    assertEquals("HELLO, FRED!", helloHandler(request).body)
-//  }
+  @Test
+  fun `can process multiple params`() {
+    val request = Request("http://www.imperial.ac.uk/say-hello?name=Fred&style=shouting")
+    assertEquals("HELLO, FRED!", helloHandler(request).body)
+  }
+
+  @Test
+  fun `can process multiple params including meaningless ones`() {
+    val request = Request("http://www.imperial.ac.uk/say-hello?name=Fred&style=shouting&thisdoesnotmean=anything")
+    assertEquals("HELLO, FRED!", helloHandler(request).body)
+  }
 
 // ***** Tests for Routing *****
 
-//  @Test
-//  fun `can route to hello handler`() {
-//    val request = Request("http://www.imperial.ac.uk/say-hello?name=Fred")
-//    assertEquals("Hello, Fred!", route(request).body)
-//  }
-//
-//  @Test
-//  fun `can route to homepage handler`() {
-//    assertEquals("This is Imperial.", route(Request("http://www.imperial.ac.uk/")).body)
-//    assertEquals("This is DoC.", route(Request("http://www.imperial.ac.uk/computing")).body)
-//  }
-//
-//  @Test
-//  fun `gives 404 when no matching route`() {
-//    assertEquals(Status.NOT_FOUND, route(Request("http://www.imperial.ac.uk/not-here")).status)
-//  }
+  @Test
+  fun `can route to hello handler`() {
+    val request = Request("http://www.imperial.ac.uk/say-hello?name=Fred")
+    assertEquals("Hello, Fred!", route(request).body)
+  }
+
+  @Test
+  fun `can route to homepage handler`() {
+    assertEquals("This is Imperial.", route(Request("http://www.imperial.ac.uk/")).body)
+    assertEquals("This is DoC.", route(Request("http://www.imperial.ac.uk/computing")).body)
+  }
+
+  @Test
+  fun `gives 404 when no matching route`() {
+    assertEquals(Status.NOT_FOUND, route(Request("http://www.imperial.ac.uk/not-here")).status)
+  }
 
 //  ***** Tests for the Extensions *****
 
 // *** More flexible routing ***
 
-//  @Test
-//  fun `calling configureRoutes() returns app which can handle requests`() {
-//
-//    val app = configureRoutes(...)
-//
-//    assertEquals("This is Imperial.", app(Request("http://www.imperial.ac.uk/")).body)
-//    assertEquals("This is DoC.", app(Request("http://www.imperial.ac.uk/computing")).body)
-//  }
+  @Test
+  fun `calling configureRoutes() returns app which can handle requests`() {
+    val app1 = configureRoutes(Request("http://www.imperial.ac.uk/"))
+    val app2 = configureRoutes(Request("http://www.imperial.ac.uk/computing"))
+    val app3 = configureRoutes(Request("http://www.imperial.ac.uk/say-hello?name=Terry&style=shouting"))
+    val app4 = configureRoutes(Request("http://www.imperial.ac.uk/not-here"))
+
+    assertEquals("This is Imperial.", app1(Request("http://www.imperial.ac.uk/")).body)
+    assertEquals("This is DoC.", app2(Request("http://www.imperial.ac.uk/computing")).body)
+    assertEquals("HELLO, TERRY!", app3(Request("http://www.imperial.ac.uk/say-hello?name=Terry&style=shouting")).body)
+    assertEquals(Status.NOT_FOUND, app4(Request("http://www.imperial.ac.uk/not_here")).status)
+  }
 
 //  *** Filters ***
 
-//  @Test
-//  fun `filter prevents access to protected resources `() {
-//
-//    val app = configureRoutes(...)
-//
-//    val request = Request("http://www.imperial.ac.uk/exam-marks")
-//    assertEquals(Status.FORBIDDEN, app(request).status)
-//  }
-//
-//  @Test
-//  fun `filter allows access to protected resources with token`() {
-//
-//    val app = configureRoutes(...)
-//
-//    val request = Request("http://www.imperial.ac.uk/exam-marks", "password1")
-//    assertEquals(Status.OK, app(request).status)
-//    assertEquals("This is very secret.", app(request).body)
-//  }
+  @Test
+  fun `filter prevents access to protected resources `() {
+
+    val request = Request("http://www.imperial.ac.uk/exam-marks")
+    val app = configureRoutes(request)
+
+    assertEquals(Status.FORBIDDEN, app(request).status)
+  }
+
+  @Test
+  fun `filter allows access to protected resources with token`() {
+    val request = Request("http://www.imperial.ac.uk/exam-marks", "password1")
+    val app = configureRoutes(request)
+
+    assertEquals(Status.OK, app(request).status)
+    assertEquals("This is very secret.", app(request).body)
+  }
 }
